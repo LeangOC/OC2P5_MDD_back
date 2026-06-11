19dbe71513ed37cbc7b582924339079b940c3805
Leang Chouteau <chouteau.leang@gmail.com>
2026-06-11 12:14:04 +0200

    maj classes

--- back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.openclassrooms.mddapi.repository;
-
-import com.openclassrooms.mddapi.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
